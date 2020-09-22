c6caa0dd053473fed86371b7652f67412e97a2f8
Jack Perala <peralaj94@gmail.com>
2020-09-22 10:34:22 -0500

    Add sorting methods of mergesort to array_merge.
    Added the sorting methods used in mergesort to the array_merge c file.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,59 @@
 #include "array_merge.h"
 
+
+bool needsSorting(int rangeSize);
+void mergeSortRange(int values[], int startIndex, int endIndex);
+void mergeRanges(int values[], int size, int startIndex, int midPoint, int endIndex);
+
+
 int* array_merge(int num_arrays, int* sizes, int** values) {
   // This is obviously broken. It has the right type, though, eh?
   return sizes;
 }
+
+void mergesort(int size, int values[]) {  
+	mergeSortRange(values, 0, size);
+}
+
+void mergeRanges(int values[], int size, int startIndex, int midPoint, int endIndex) {
+	int rangeSize = endIndex - startIndex;
+	int* destination;
+	destination = (int*) calloc(size + 1, sizeof(int));
+	int firstIndex = startIndex;
+	int secondIndex = midPoint;
+	int copyIndex = 0;
+	
+	while (firstIndex < midPoint && secondIndex < endIndex) {
+		if (values[firstIndex] < values[secondIndex]) {
+			destination[copyIndex] = values[firstIndex];
+			firstIndex++;
+		}else{
+			destination[copyIndex] = values[secondIndex];
+			secondIndex++;
+		}
+		copyIndex++;
+	}
+
+	while (firstIndex < midPoint) {
+		destination[copyIndex] = values[firstIndex];
+		copyIndex++;
+		firstIndex++;
+	}
+
+	while (secondIndex < endIndex) {
+		destination[copyIndex] = values[secondIndex];
+		copyIndex++;
+		secondIndex++;
+	}
+
+	for (int i = 0; i < rangeSize; i++) {
+		values[i + startIndex] = destination[i];
+	}
+	free(destination);
+}
+
+bool needsSorting (int rangeSize) {
+	if (rangeSize >= 2) {
+		return  true;
+	}else{
+		return false;
