abfdc947f243c33003dd0ffff19c60e5ddff270b
Jack Perala <peralaj94@gmail.com>
2020-09-21 21:41:20 -0500

    Write the mergeRanges piece of mergeSort
    Wrote the mergeRanges component of mergeSort.

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,7 +1,45 @@
 #include "mergesort.h"
+#include <stdlib.h>
 
-void mergesort(int, int*) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
-  return;
+void mergesort(int, int*) {  
+
+	return;
 }
+
+void mergeRanges(int values[], int size, int startIndex, int midPoint, int endIndex) {
+	int rangeSize = endIndex - startIndex;
+	int* destination;
+	destination = (int*) malloc(size + 1, sizeof(int));
+	int firstIndex = startIndex;
+	int secondIndex = midPoint;
+	int copyIndex = 0;
+	
+	while (firstIndex < midPoint && secondIndex < endIndex) {
+		if (values[firstIndex] < values[secondIndex]) {
+			destination[copyIndex] = values[firstIndex];
+			firstIndex++;
+		}else{
+			destination[copyIndex] = values[secondIndex];
+			secondIndex++;
+		}
+		copyIndex++;
+	}
+
+	while (firstIndex < midPoint) {
+		destination[copyIndex] = values[firstIndex];
+		copyIndex++;
+		firstIndex++;
+	}
+
+	while (secondIndex < endIndex) {
+		destination[copyIndex] = values[secondIndex];
+		copyIndex++;
+		secondIndex++;
+	}
+
+	for (int i = 0; i < rangeSize; i++) {
+		values[i + startIndex] = destination[i];
+	}
+	free(destination);
+}
+
