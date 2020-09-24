9fb5419bdf4b04ea81d5a2b501a5020bd0ce534f
Jack Perala <peralaj94@gmail.com>
2020-09-24 11:02:08 -0500

    Remove the unecessary mergesort functions
    Removed the mergesort functions I brought over. After some research using the correct includes should allow me to access the function.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -2,59 +2,23 @@
 #include "../mergesort/mergesort.h"
 #include "../mergesort/mergesort.c"
 
-bool needsSorting(int rangeSize);
-void mergeSortRange(int values[], int startIndex, int endIndex);
-void mergeRanges(int values[], int size, int startIndex, int midPoint, int endIndex);
-
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
+	
+	int* combArray
+	
+	int i = 0;
+
+	// While loop to add the sum of elements in the original arrays
+	while (i < num_arrays){
+		combArray = combArray + size[i];
+		i++;
+	}
+	free(i); // Free the iteration variable, maybe go back and change this
+		 // to a for-loop.
+	
+
   return sizes;
 }
 
-void mergesort(int size, int values[]) {  
-	mergeSortRange(values, 0, size);
-}
 
-void mergeRanges(int values[], int size, int startIndex, int midPoint, int endIndex) {
-	int rangeSize = endIndex - startIndex;
-	int* destination;
-	destination = (int*) calloc(size + 1, sizeof(int));
-	int firstIndex = startIndex;
-	int secondIndex = midPoint;
-	int copyIndex = 0;
-	
-	while (firstIndex < midPoint && secondIndex < endIndex) {
-		if (values[firstIndex] < values[secondIndex]) {
-			destination[copyIndex] = values[firstIndex];
-			firstIndex++;
-		}else{
-			destination[copyIndex] = values[secondIndex];
-			secondIndex++;
-		}
-		copyIndex++;
-	}
-
-	while (firstIndex < midPoint) {
-		destination[copyIndex] = values[firstIndex];
-		copyIndex++;
-		firstIndex++;
-	}
-
-	while (secondIndex < endIndex) {
-		destination[copyIndex] = values[secondIndex];
-		copyIndex++;
-		secondIndex++;
-	}
-
-	for (int i = 0; i < rangeSize; i++) {
-		values[i + startIndex] = destination[i];
-	}
-	free(destination);
-}
-
-bool needsSorting (int rangeSize) {
-	if (rangeSize >= 2) {
-		return  true;
-	}else{
-		return false;
