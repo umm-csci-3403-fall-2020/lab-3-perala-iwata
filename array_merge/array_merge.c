ce00f92f49a840c755c24eb7e4b118d537f7f58f
Jack Perala <peralaj94@gmail.com>
2020-09-25 15:49:19 -0500

    Create framework for array_merge
    After scrapping the old line of thinking I have created the framework for the array_merge portion. Definitely needs some debugging.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,12 +1,15 @@
 #include "array_merge.h"
 #include "../mergesort/mergesort.h"
-#include "../mergesort/mergesort.c"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
 	
+	// Variable used to see how many total elements there are in all sub-arrays
 	int* combArray
 	
+	// Index to count
 	int i = 0;
 
 	// While loop to add the sum of elements in the original arrays
@@ -14,11 +17,55 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
 		combArray = combArray + size[i];
 		i++;
 	}
-	free(i); // Free the iteration variable, maybe go back and change this
-		 // to a for-loop.
 	
+	// Array to hold the unique values of the subarrays
+	int *altArray = (int*) calloc(combArray, sizeof(int));
+	
+	int index = 0;
 
-  return sizes;
+	for(int i = 0; i < num_arrays; i++){
+		for(int j = 0; j < sizes[i]; j++){
+			combArray[index] = values[j][i];
+			index++;
+		}
+	}
+
+	// Sorting the array to find unique numbers
+	mergesort(combArray, altArray);
+	
+	// Counter for unique values
+	int unique = 0;
+
+	// Iterator for the while loop
+	int j = 0;
+
+	while(j < combArray) {
+		if(altArray[i] != altArray[i - 1]){
+			unique++;
+		}
+	}
+
+	// Final array to hold the unique values as well as the count of unique values
+	int* finalArray = (int*) calloc(unique+1, sizeof(int));
+
+	// Adding the value of unique numbers to the first position in the array
+	finalArray[0] = unique;
+
+	for(int k = 1; i < combArray; i++) {
+		if (combArray[i] != combArray[i-1]){
+			finalArray[k] = altArray[k];
+		}
+	}
+	free(combArray);
+  return finalArray;
 }
 
-
+/* What to do next:
+Fill the forloop on line 24 to fill the altArray with unique values.
+Sort the array
+Create a new array with space in the beginning to hold the count for the number of unique values.
+Set the new array at index 0 to be the value of the unique values.
+Fill the new array with the unique values starting at posistion one.
+Be sure to free the new array.
+Return the finalized sorted array.
+Done?
