0988f2ce674274ea83f916cb409f2c7868a6398a
Jack Perala <peralaj94@gmail.com>
2020-09-25 15:57:35 -0500

    Change a while loop to for loop
    Changed a while loop to a for loop for clarity and to eliminate an error.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -9,11 +9,8 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
 	// Variable used to see how many total elements there are in all sub-arrays
 	int* combArray
 	
-	// Index to count
-	int i = 0;
-
 	// While loop to add the sum of elements in the original arrays
-	while (i < num_arrays){
+	for (int i = 0; i < num_arrays; i++){
 		combArray = combArray + size[i];
 		i++;
 	}
@@ -23,7 +20,7 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
 	
 	int index = 0;
 
-	for(int i = 0; i < num_arrays; i++){
+	for (int i = 0; i < num_arrays; i++){
 		for(int j = 0; j < sizes[i]; j++){
 			combArray[index] = values[j][i];
 			index++;
@@ -39,7 +36,7 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
 	// Iterator for the while loop
 	int j = 0;
 
-	while(j < combArray) {
+	while (j < combArray) {
 		if(altArray[i] != altArray[i - 1]){
 			unique++;
 		}
@@ -59,13 +56,3 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
 	free(combArray);
   return finalArray;
 }
-
-/* What to do next:
-Fill the forloop on line 24 to fill the altArray with unique values.
-Sort the array
-Create a new array with space in the beginning to hold the count for the number of unique values.
-Set the new array at index 0 to be the value of the unique values.
-Fill the new array with the unique values starting at posistion one.
-Be sure to free the new array.
-Return the finalized sorted array.
-Done?
